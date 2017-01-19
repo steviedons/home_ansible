d53b3630be7d10d334849ab429659c1bdcd63bc3
Stephen Donovan <steviedons@gmail.com>
2017-01-19 22:15:20 +0000

    Updated the crypt password script to include that the passlib needs to be installed

--- crypt_passwd.py
+++ crypt_passwd.py
@@ -1,3 +1,6 @@
+# This python script is for creating passwords to use in playbooks.
+# need to install python-passlib
+
 from passlib.hash import sha512_crypt
 import sys
 hash = sha512_crypt.encrypt(sys.argv[1])
